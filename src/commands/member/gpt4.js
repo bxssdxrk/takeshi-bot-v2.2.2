1bebc4b7af66af0b23d2910a5aa543a301f51625
Bass Dark <72193669+bxssdxrk@users.noreply.github.com>
2024-11-12 03:24:29 -0300

    GPT-4 Sem a API SpiderX

---
+++ src/commands/member/gpt4.js
@@ -0,0 +1,29 @@
+const { PREFIX } = require("../../config");
+const { Hercai } = require("hercai");
+const { WarningError } = require("../../errors/WarningError");
+
+module.exports = {
+  name: "GPT4",
+  description: "Envie uma mensagem ao GPT-4.",
+  commands: ["gpt4"],
+  usage: `${PREFIX}gpt4 <mensagem>`,
+  handle: async ({
+    fullArgs,
+    sendImageFromURL,
+    sendSuccessReply,
+  }) => {
+    await sendWaitReact();
+    const herc = new Hercai();
+    
+    if (!fullArgs.length) {
+      throw new WarningError(
+        "Por favor, forneça uma mensagem."
+      );
+    }
+    
+    const response = await herc.question({
+      content: `${fullArgs}`,
+    });
+    await sendSuccessReply(response.reply);
+  },
+};
